54263a17ab33b988e7277c41593611d366a3149b
Pedro Henrique Ribeiro Cortez <pedrocortez877@gmail.com>
2021-05-04 17:21:40 -0300

    Criação do componente de menu e inicialização da transição entre telas

--- app/src/main/java/com/example/pizzeriadommortandello/RegisterPizzaActivity.java
+++ app/src/main/java/com/example/pizzeriadommortandello/RegisterPizzaActivity.java
@@ -3,21 +3,12 @@ package com.example.pizzeriadommortandello;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterPizzaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void onClickPizzaRegister(View view){
-        
-    }
-
-    public void onClickMenu(View view){
-
+        setContentView(R.layout.activity_register_pizza);
     }
 }
